698aebea623102a3052fa7a1b25f7bbca10f7ae1
Danishjamil01 <danishjamil9065@gmail.com>
2024-10-26 00:36:04 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ayeshaparween';
+
+
+  isBotOpen = false;
+
+  toggleBot() {
+    this.isBotOpen = !this.isBotOpen;
+  }
+
+
+  ngAfterViewInit(): void {
+    this.autoPlayOnMobile();
+  }
+
+  autoPlayOnMobile() {
+    const videoElement: HTMLVideoElement = document.querySelector('video');
+    if (videoElement) {
+      // Check if the video is muted and autoplay is allowed
+      videoElement.muted = true;
+      videoElement.play().catch((error) => {
+        console.log('Autoplay was prevented:', error);
+        // Optionally show a play button to the user
+      });
+    }
+  }
 }
